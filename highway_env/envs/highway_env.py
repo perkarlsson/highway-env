2be76fd1b52c9341434570e6084b783e8c2305f5
Per Karlsson <per@pkarlsson.com>
2021-05-27 14:41:23 -0700

    Update highway_env.py
    updated again, using code from roundabout example

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -75,32 +75,30 @@ class HighwayEnv(AbstractEnv):
                     other_vehicles_type.create_random(self.road, spacing=1 / self.config["vehicles_density"])
                 )
 
+    Modifications to original reward function to allow lane_change_reward
+
     def _reward(self, action: Action) -> float:
-        """
-        The reward is defined to foster driving at high speed, on the rightmost lanes, and to avoid collisions.
-        :param action: the last action performed
-        :return: the corresponding reward
-        """
-        neighbours = self.road.network.all_side_lanes(self.vehicle.lane_index)
-        lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
-            else self.vehicle.lane_index[2]
-        scaled_speed = utils.lmap(self.vehicle.speed, self.config["reward_speed_range"], [0, 1])
-        # new line, from roundabout example
-        lane_change = action == 0 or action == 2
-        reward = \
-            # new line
-            self.config["lane_change_reward"] * lane_change \    
-            #
-            + self.config["collision_reward"] * self.vehicle.crashed \
-            + self.config["right_lane_reward"] * lane / max(len(neighbours) - 1, 1) \
-            + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1)
-        reward = utils.lmap(reward,
-                          #[self.config["collision_reward"],
-                          [self.config["collision_reward"] + self.config["lane_change_reward"],
-                          self.config["high_speed_reward"] + self.config["right_lane_reward"]],
-                          [0, 1])
-        reward = 0 if not self.vehicle.on_road else reward
-        return reward
+            """
+            The reward is defined to foster driving at high speed, on the rightmost lanes, and to avoid collisions.
+            :param action: the last action performed
+            :return: the corresponding reward
+            """
+            neighbours = self.road.network.all_side_lanes(self.vehicle.lane_index)
+            lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
+                else self.vehicle.lane_index[2]
+            scaled_speed = utils.lmap(self.vehicle.speed, self.config["reward_speed_range"], [0, 1])
+            lane_change = action == 0 or action == 2  ############## from roundabout example
+            reward = \
+                self.config["lane_change_reward"] * lane_change \    ############## from roundabout example
+                + self.config["collision_reward"] * self.vehicle.crashed \
+                + self.config["right_lane_reward"] * lane / max(len(neighbours) - 1, 1) \
+                + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1)
+            reward = utils.lmap(reward,
+                              [self.config["collision_reward"] + self.config["lane_change_reward"], ############## from roundabout
+                              self.config["high_speed_reward"] + self.config["right_lane_reward"]],
+                              [0, 1])
+            reward = 0 if not self.vehicle.on_road else reward
+            return reward
 
     def _is_terminal(self) -> bool:
         """The episode is over if the ego vehicle crashed or the time is out."""
