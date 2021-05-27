86a11dfcf4063a7d8e6e80793154052be99beae2
Per Karlsson <per@pkarlsson.com>
2021-05-27 14:15:30 -0700

    Update highway_env.py
    added lane_change

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -85,11 +85,17 @@ class HighwayEnv(AbstractEnv):
         lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
             else self.vehicle.lane_index[2]
         scaled_speed = utils.lmap(self.vehicle.speed, self.config["reward_speed_range"], [0, 1])
+        # new line
+        lane_change = {0: self.LANE_CHANGE_REWARD, 1: 0, 2: self.LANE_CHANGE_REWARD, 3: 0, 4: 0}
         reward = \
+            # new line
+            + self.config["lane_change_reward"] * lane_change \    
+            #
             + self.config["collision_reward"] * self.vehicle.crashed \
             + self.config["right_lane_reward"] * lane / max(len(neighbours) - 1, 1) \
             + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1)
-        reward = utils.lmap(reward,
+        #reward = utils.lmap(reward,
+        reward = utils.lmap(lane_change[action] + reward,
                           [self.config["collision_reward"],
                            self.config["high_speed_reward"] + self.config["right_lane_reward"]],
                           [0, 1])
