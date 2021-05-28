de79f5fd73fceca9367b59ae6f7f771f5aedc0f5
Per Karlsson <per@pkarlsson.com>
2021-05-28 15:23:00 -0700

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -85,14 +85,14 @@ class HighwayEnv(AbstractEnv):
             lane = self.vehicle.target_lane_index[2] if isinstance(self.vehicle, ControlledVehicle) \
                 else self.vehicle.lane_index[2]
             scaled_speed = utils.lmap(self.vehicle.speed, self.config["reward_speed_range"], [0, 1])
-            lane_change = action == 0 or action == 2  ############## from roundabout and merge examples
+            lane_change = action == 0 or action == 2  # from roundabout and merge examples
             reward = \
-                self.config["lane_change_reward"] * lane_change \    ############## from roundabout and merge examples
+                self.config["lane_change_reward"] * lane_change \   # from roundabout and merge examples
                 + self.config["collision_reward"] * self.vehicle.crashed \
                 + self.config["right_lane_reward"] * lane / max(len(neighbours) - 1, 1) \
                 + self.config["high_speed_reward"] * np.clip(scaled_speed, 0, 1)
             reward = utils.lmap(reward,
-                              [self.config["collision_reward"] + self.config["lane_change_reward"], ############## from roundabout and merge examples
+                              [self.config["collision_reward"] + self.config["lane_change_reward"],  # from roundabout and merge examples
                               self.config["high_speed_reward"] + self.config["right_lane_reward"]],
                               [0, 1])
             reward = 0 if not self.vehicle.on_road else reward
